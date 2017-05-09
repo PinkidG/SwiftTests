d66f15b20392c40d4c48815e14e7a9e48895f682
PinkidG <pppinki@icloud.com>
2017-05-09 10:59:42 +0200

    Initial Commit

---
+++ SwiftTests/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftTests
+//
+//  Created by Philipp Pinkernelle on 09.05.17.
+//  Copyright © 2017 Philipp Pinkernelle. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
